b238a5f6247982bbdd195a741a605842d932d414
Tyler Leonhardt <tylerl0706@gmail.com>
2016-01-28 14:34:21 -0500

    added new name

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var TOKENS = require('./tokens.js');
-var api = require('instagram-node').instagram();
+var instagram = require('instagram-node');
+var api = instagram.instagram();
 
 
 api.use({ client_id: TOKENS.insta.client_id,
@@ -17,28 +18,42 @@ app.use('/go', express.static('go'));
 var Clarifai = require('./clarifai_node.js');
 Clarifai.initAPI(TOKENS.clarifai.client_id, TOKENS.clarifai.client_secret );
 
-var redirect_uri = 'http://instatype.me/go';
+var redirect_uri = 'http://localhost:3000/go';
 
 exports.authorize_user = function(req, res) {
-  res.redirect(api.get_authorization_url(redirect_uri, { scope: ['public_content'] }));
+  // var user = "";
+  // if(req.query.user){
+  //   user = "?user=" + req.query.user;
+  // }
+  res.redirect(api.get_authorization_url(redirect_uri, {}));
 };
 
 exports.handleauth = function(req, res) {
+  // var user = "";
+  // if(req.query.user){
+  //   user = "?user=" + req.query.user;
+  // }
   api.authorize_user(req.query.code, redirect_uri, function(err, result) {
-      console.log('Yay! Access token is ' + result.access_token);
-      api.use({ access_token: result.access_token });
-      console.log(JSON.stringify(result));
+      if(err){
+        console.log(err);
+        res.json(err);
+        return;
+      }
+      // console.log('Yay! Access token is ' + result.access_token);
+      var insta = instagram.instagram();
+      insta.use({ access_token: result.access_token });
+      // console.log(JSON.stringify(result));
 
       var date = new Date();
       var options = {
         max_timestamp: Date.now(),
         min_timestamp: new Date(new Date().setYear(new Date().getFullYear() + 1))
       }
-      /* OPTIONS: { [count], [min_timestamp], [max_timestamp], [min_id], [max_id] }; */
-      api.user_media_recent(result.user.id, options, function(err, medias, pagination, remaining, limit) {
+      // var un = req.query.user ? req.query.user : result.user.id;
 
-        api.use({ client_id: TOKENS.insta.client_id,
-                 client_secret: TOKENS.insta.client_secret });
+      /* OPTIONS: { [count], [min_timestamp], [max_timestamp], [min_id], [max_id] }; */
+      insta.user_media_recent(result.user.id, options, function(err, medias, pagination, remaining, limit) {
+
         var urlArr = medias.map(function(media){
           if(media['videos']){
             return media['videos']['standard_resolution']['url'];
@@ -60,14 +75,14 @@ exports.handleauth = function(req, res) {
 
           var morePromises = [];
 
-          console.log(r);
+          // console.log(r);
 
           for(var i = 0; i < r.length; i++) {
             var personality = [];
-            console.log(i);
+            // console.log(i);
             var theText = r[i].join(" ");
 
-            console.log(medias[i].caption);
+            // console.log(medias[i].caption);
             // console.log(medias[i].caption.text);
 
             if(medias[i].caption ) {
@@ -80,32 +95,14 @@ exports.handleauth = function(req, res) {
             personality.push(getJudgingPerceiving(theText));
             personality.push(getMood(theText));
             personality.push(getSentiment(theText));
-            console.log(theText);
+            // console.log(theText);
             morePromises.push(Promise.all(personality));
           }
 
-          console.log("hello");
+          // console.log("hello");
 
-          // r.forEach(function(arr, index){
-          //   var personality = [];
-          //   var theText = arr.join(" ");
-          //   theText = theText + " " + trimTweet(medias[index].caption.text);
-          //   console.log(medias[index].caption.text);
-          //   console.log(index);
-          //   personality.push(getSensingIntuition(theText));
-          //   personality.push(getThinkingFeeling(theText));
-          //   personality.push(getExtraIntro(theText));
-          //   personality.push(getJudgingPerceiving(theText));
-          //   personality.push(getMood(theText));
-          //   personality.push(getSentiment(theText));
-          //
-          //   morePromises.push(Promise.all(personality));
-          //
-          //   console.log("hello");
-          // });
-          // console.log("hey");
           Promise.all(morePromises).then(function(theBigArray) {
-            console.log("yo");
+            // console.log("yo");
             // console.log(JSON.stringify(theBigArray));
             theBigArray = theBigArray.map(function(p){
               var display = {};
@@ -127,7 +124,7 @@ exports.handleauth = function(req, res) {
               return (previousValue.happy > currentValue.happy) ? previousValue:currentValue;
             })));
 
-            console.log("THIS IS THE HAPPY" + happyIndex);
+            // console.log("THIS IS THE HAPPY" + happyIndex);
 
 
 
@@ -136,41 +133,10 @@ exports.handleauth = function(req, res) {
               emotions:theBigArray,
               happiest:happyIndex
             }
-            // res.contentType('json');
-            // res.type('json');
             res.json(sendThis);
             // api.use({ client_id: '9ba26a3c029f4d918f5619a8eaafd80c',
             //          client_secret: '345ef7f5f4e043a2bbc55f2fae13b39a' });
           });
-
-          // var flattened = [];
-          // r.forEach(function(arr){
-          //   flattened = flattened.concat(arr);
-          // });
-          //
-          // var personality = [];
-          // var display = {};
-          // var allText = flattened.join(" ");
-          // personality.push(getSensingIntuition(allText));
-          // personality.push(getThinkingFeeling(allText));
-          // personality.push(getExtraIntro(allText));
-          // personality.push(getJudgingPerceiving(allText));
-          // personality.push(getMood(allText));
-          // personality.push(getSentiment(allText));
-          //
-          // Promise.all(personality).then(function(p) {
-          //   console.log(p);
-          //   var allP = [];
-          //   p.forEach(function(obj){
-          //     for(var attrname in obj) { display[attrname] = obj[attrname]; }
-          //   });
-          //   console.log("~~~");
-          //   console.log(display);
-          //
-          //   res.send(JSON.stringify(display));
-          //   api.use({ client_id: '9ba26a3c029f4d918f5619a8eaafd80c',
-          //            client_secret: '345ef7f5f4e043a2bbc55f2fae13b39a' });
-          // });
         });
       });
   });
@@ -190,7 +156,10 @@ var getTagsByURL = function(url) {
     Clarifai.tagURL( url, null, "", function(error, response){
         //console.log(JSON.stringify(response));
         // console.log(JSON.stringify(response));
-        if(error)console.log(error);
+        if(error){
+          console.log(error);
+          reject(error);
+        }
         var list = response.results[0].result.tag.classes;
 
         if(Array.isArray(list[0])) {
@@ -208,108 +177,114 @@ var getTagsByURL = function(url) {
 
 var getSensingIntuition = function(text) {
   return new Promise(function(resolve, reject) {
-    console.log(text);
+    // console.log(text);
     rp('http://uclassify.com/browse/prfekt/myers-briggs-perceiving-function/ClassifyText?readkey='+TOKENS.uclassify.read_key+'&version=1.01&output=json&text='+text)
     .then(function (response) {
       if(typeof response == "string")
         response = JSON.parse(response);
         // Process html...
-      console.log(JSON.stringify(response.cls1));
+      // console.log(JSON.stringify(response.cls1));
       resolve({ "sensing": response.cls1.Sensing, "intuition": response.cls1.iNtuition });
     })
     .catch(function (err) {
         // Crawling failed...
         console.log(err);
+        reject(err);
     });
   });
 };
 
 var getThinkingFeeling = function(text) {
   return new Promise(function(resolve, reject) {
-    console.log(text);
+    // console.log(text);
     rp('http://uclassify.com/browse/prfekt/myers-briggs-judging-function/ClassifyText?readkey='+TOKENS.uclassify.read_key+'&version=1.01&output=json&text='+text)
     .then(function (response) {
       if(typeof response == "string")
         response = JSON.parse(response);
         // Process html...
-      console.log(JSON.stringify(response.cls1));
+      // console.log(JSON.stringify(response.cls1));
       resolve({ "feeling": response.cls1.Feeling, "thinking": response.cls1.Thinking });
     })
     .catch(function (err) {
         // Crawling failed...
         console.log(err);
+        reject(err);
     });
   });
 };
 
 var getExtraIntro = function(text) {
   return new Promise(function(resolve, reject) {
-    console.log(text);
+    // console.log(text);
     rp('http://uclassify.com/browse/prfekt/myers-briggs-attitude/ClassifyText?readkey='+TOKENS.uclassify.read_key+'&version=1.01&output=json&text='+text)
     .then(function (response) {
       if(typeof response == "string")
         response = JSON.parse(response);
         // Process html...
-      console.log(JSON.stringify(response.cls1));
+      // console.log(JSON.stringify(response.cls1));
       resolve({ "extraversion": response.cls1.Extraversion, "introversion": response.cls1.Introversion });
     })
     .catch(function (err) {
         // Crawling failed...
         console.log(err);
+        reject(err);
     });
   });
 };
 
 var getJudgingPerceiving = function(text) {
   return new Promise(function(resolve, reject) {
-    console.log(text);
+    // console.log(text);
     rp('http://uclassify.com/browse/prfekt/myers-briggs-lifestyle/ClassifyText?readkey='+TOKENS.uclassify.read_key+'&version=1.01&output=json&text='+text)
     .then(function (response) {
       if(typeof response == "string")
         response = JSON.parse(response);
         // Process html...
-      console.log(JSON.stringify(response.cls1));
+      // console.log(JSON.stringify(response.cls1));
       resolve({ "judging": response.cls1.Judging, "perceiving": response.cls1.Perceiving });
     })
     .catch(function (err) {
         // Crawling failed...
         console.log(err);
+        reject(err);
     });
   });
 };
 
 var getMood = function(text) {
   return new Promise(function(resolve, reject) {
-    console.log(text);
+    // console.log(text);
     rp('http://uclassify.com/browse/prfekt/mood/ClassifyText?readkey='+TOKENS.uclassify.read_key+'&version=1.01&output=json&text='+text)
     .then(function (response) {
       if(typeof response == "string")
         response = JSON.parse(response);
         // Process html...
-      console.log(JSON.stringify(response.cls1));
+      // console.log(JSON.stringify(response.cls1));
       resolve({ "happy": response.cls1.happy, "upset": response.cls1.upset });
     })
     .catch(function (err) {
         // Crawling failed...
         console.log(err);
+        reject(err);
     });
   });
 };
 
 var getSentiment = function(text) {
   return new Promise(function(resolve, reject) {
-    console.log(text);
+    // console.log(text);
     rp('http://uclassify.com/browse/uclassify/sentiment/ClassifyText?readkey='+TOKENS.uclassify.read_key+'&version=1.01&output=json&text='+text)
     .then(function (response) {
       if(typeof response == "string")
         response = JSON.parse(response);
         // Process html...
-      console.log(JSON.stringify(response.cls1));
+      // console.log(JSON.stringify(response.cls1));
       resolve({ "negative": response.cls1.negative, "positive": response.cls1.positive });
     })
     .catch(function (err) {
         // Crawling failed...
         console.log(err);
+        reject(err);
     });
   });
 };
@@ -341,5 +316,5 @@ function max(search){
     return (previousValue.happy > currentValue.happy) ? previousValue:currentValue;
   })));
 
-  console.log("THIS IS THE "  + happyIndex);
+  // console.log("THIS IS THE "  + happyIndex);
 }
